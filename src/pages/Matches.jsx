40e1f8d7fb37647d1227b67e7fea957ede0a7fb0
Caleb Hemphill <calebhemp@gmail.com>
2023-12-20 12:21:14 -0500

    Add request to backend for matches data

--- src/pages/Matches.jsx
+++ src/pages/Matches.jsx
@@ -1,3 +1,7 @@
+// react
+import { useEffect, useState } from 'react';
+// dependencies
+import axios from 'axios';
 // bootstrap
 import Stack from 'react-bootstrap/Stack';
 import Col from 'react-bootstrap/Col';
@@ -7,20 +11,64 @@ import InputGroup from 'react-bootstrap/InputGroup';
 import Table from 'react-bootstrap/Table';
 
 function Matches() {
+
+  const teamId = 61; //temp, replace with state from parent
+
+  const [results, setResults] = useState({});
+  const [matches, setMatches] = useState([]);
+  const [allMatches, setAllMatches] = useState([]);
+  const [pastMatches, setPastMatches] = useState([]);
+  const [futureMatches, setFutureMatches] = useState([]);
+  const [activeMatches, setActiveMatches] = useState([]);
+
+  // useEffect(() => {
+  //   getMatches();
+  // });
+
+  async function getMatches() {
+    const url = `http://localhost:3001/matches/team/${teamId}`;
+
+    try {
+
+      const response = await axios.get(url);
+      const resultsResponse = response.data.results;
+      const matchesResponse = response.data.matches;
+      setResults(resultsResponse);
+      setMatches(matchesResponse);
+      sortMatchesData();
+
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
+
+  function sortMatchesData() {
+    if (matches.active.length) {
+      setPastMatches(matches.past);
+      setFutureMatches(matches.future);
+      setActiveMatches(matches.active);
+      setAllMatches([...matches.active, ...matches.past, ...matches.future]);
+    } else {
+      setPastMatches(matches.past);
+      setFutureMatches(matches.future);
+      setAllMatches([...matches.past, ...matches.future]);
+    }
+  }
+
   return (
     <Stack>
 
       <MatchesForm />
 
-      <Table>
+      <Table size='sm'>
         <thead>
           <tr>
             <th>Status</th>
             <th>Date</th>
             <th>Matchday</th>
-            <th>Home Team</th>
+            <th>Home</th>
             <th>Score</th>
-            <th>Away Team</th>
+            <th>Away</th>
             <th>Score</th>
           </tr>
         </thead>
@@ -31,11 +79,17 @@ function Matches() {
 }
 
 function MatchesForm() {
+
+  const [status, setStatus] = useState();
+  const [season, setSeason] = useState();
+  const [dateFrom, setDateFrom] = useState();
+  const [dateTo, setDateTo] = useState();
+
   return (
     <Form>
       <Row>
         <Col>
-          <InputGroup>
+          <InputGroup size='sm'>
             <InputGroup.Text>Status</InputGroup.Text>
             <Form.Select>
               <option value="all">All</option>
@@ -47,7 +101,7 @@ function MatchesForm() {
           </InputGroup>
         </Col>
         <Col>
-          <InputGroup>
+          <InputGroup size='sm'>
             <InputGroup.Text>Season</InputGroup.Text>
             <Form.Select>
               <option value="all">2023</option>
@@ -59,7 +113,7 @@ function MatchesForm() {
           </InputGroup>
         </Col>
         <Col>
-          <InputGroup>
+          <InputGroup size='sm'>
             <InputGroup.Text>From</InputGroup.Text>
             <Form.Control type='date'></Form.Control>
             <InputGroup.Text>To</InputGroup.Text>
