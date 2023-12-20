80b464397c5c32e79bef0231681053eb0e171316
ARMANDO BUGARIN <armando.bugarin24@gmail.com>
2023-12-20 15:19:38 -0800

    edit carousel

--- src/components/HeroImage.jsx
+++ src/components/HeroImage.jsx
@@ -1,4 +1,4 @@
-import futbol1 from "/img/futbol1.jpg";
+import futbol5 from "/img/futbol5.png";
 import futbol4 from "/img/futbol4.jpeg";
 import futbol3 from "/img/futbol3.jpg";
 import Carousel from "react-bootstrap/Carousel";
@@ -6,15 +6,15 @@ import Carousel from "react-bootstrap/Carousel";
 export default function HeroImage() {
     return (
         <Carousel
-            interval={5000}
-            style={{ maxHeight: '400px', overflow: 'hidden' }}
+            interval={5000} slide={false}
+            style={{ height: '400px', maxWidth: '800px', overflow: 'hidden' }}
         >
             <Carousel.Item>
                 <img
                     className="d-block w-100"
-                    src={futbol1}
+                    src={futbol5}
                     alt="Lionel Messi"
-                    style={{ objectFit: 'cover', maxHeight: '400px', margin: 'auto' }}
+                    style={{ objectFit: 'cover', height: '400px', width: 'auto', margin: 'auto' }}
                 />
             </Carousel.Item>
             <Carousel.Item>
@@ -22,7 +22,7 @@ export default function HeroImage() {
                     className="d-block w-100"
                     src={futbol4}
                     alt="Alexis Morgan"
-                    style={{ objectFit: 'cover', maxHeight: '400px', margin: 'auto' }}
+                    style={{ objectFit: 'cover', height: '400px', width: 'auto', margin: 'auto' }}
                 />
             </Carousel.Item>
             <Carousel.Item>
@@ -30,7 +30,7 @@ export default function HeroImage() {
                     className="d-block w-100"
                     src={futbol3}
                     alt="Kylian Mbappe"
-                    style={{ objectFit: 'cover', maxHeight: '400px', margin: 'auto' }}
+                    style={{ objectFit: 'cover', height: '400px', width: 'auto', margin: 'auto' }}
                 />
             </Carousel.Item>
         </Carousel>
