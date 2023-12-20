4ae516702077d59598bd6753fd482c6bc6042acb
Caleb Hemphill <calebhemp@gmail.com>
2023-12-20 11:12:31 -0500

    Initial layout table/form

--- src/pages/Matches.jsx
+++ src/pages/Matches.jsx
@@ -1,9 +1,75 @@
-import React from'react';
+// bootstrap
+import Stack from 'react-bootstrap/Stack';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
+import Table from 'react-bootstrap/Table';
 
-export default function Matches() {
-    return (
-        <div>
-            <h1>Matches</h1>
-        </div>
-    );
+function Matches() {
+  return (
+    <Stack>
+
+      <MatchesForm />
+
+      <Table>
+        <thead>
+          <tr>
+            <th>Status</th>
+            <th>Date</th>
+            <th>Matchday</th>
+            <th>Home Team</th>
+            <th>Score</th>
+            <th>Away Team</th>
+            <th>Score</th>
+          </tr>
+        </thead>
+      </Table>
+
+    </Stack>
+  )
 }
+
+function MatchesForm() {
+  return (
+    <Form>
+      <Row>
+        <Col>
+          <InputGroup>
+            <InputGroup.Text>Status</InputGroup.Text>
+            <Form.Select>
+              <option value="all">All</option>
+              <option value="past">Finished</option>
+              <option value="future">Scheduled</option>
+              <option value="active">In Play</option>
+            </Form.Select>
+            <InputGroup.Checkbox></InputGroup.Checkbox>
+          </InputGroup>
+        </Col>
+        <Col>
+          <InputGroup>
+            <InputGroup.Text>Season</InputGroup.Text>
+            <Form.Select>
+              <option value="all">2023</option>
+              <option value="past">2022</option>
+              <option value="future">2021</option>
+              <option value="active">2020</option>
+            </Form.Select>
+            <InputGroup.Checkbox></InputGroup.Checkbox>
+          </InputGroup>
+        </Col>
+        <Col>
+          <InputGroup>
+            <InputGroup.Text>From</InputGroup.Text>
+            <Form.Control type='date'></Form.Control>
+            <InputGroup.Text>To</InputGroup.Text>
+            <Form.Control type='date'></Form.Control>
+            <InputGroup.Checkbox></InputGroup.Checkbox>
+          </InputGroup>
+        </Col>
+      </Row>
+    </Form>
+  )
+}
+
+export default Matches;
