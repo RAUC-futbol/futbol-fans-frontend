8482d431a7c1b9867333fd17560793c7c8d4b17e
Caleb Hemphill <calebhemp@gmail.com>
2023-12-21 17:13:20 -0500

    Update profile page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,3 +1,5 @@
+// react
+import { useState } from 'react';
 // dependencies
 import axios from 'axios';
 // bootstrap
@@ -9,14 +11,18 @@ import ProfileForm from '../components/ProfileForm';
 
 function Profile({ user, updateUser }) {
 
+  const [currentUser, setCurrentUser] = useState(user);
+
   async function updateUserProfile(userProfile) {
     const url = `${SERVER}/users/${user._id}`;
 
     try {
 
       const response = await axios.put(url, userProfile);
+      const updatedUser = response.data;
 
-      updateUser(response.data);
+      setCurrentUser(updatedUser);
+      updateUser(updatedUser);
 
     } catch (error) {
       console.error(error.message);
@@ -29,7 +35,7 @@ function Profile({ user, updateUser }) {
 
       <h2>Profile</h2>
 
-      <ProfileForm user={user} handleSubmit={updateUserProfile} />
+      <ProfileForm user={currentUser} handleSubmit={updateUserProfile} />
 
     </Container>
   );
