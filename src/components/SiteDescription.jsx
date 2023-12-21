e933c0d0b50d3c4226fd6ba84b182a659d7cce5a
ARMANDO BUGARIN <armando.bugarin24@gmail.com>
2023-12-20 20:09:22 -0800

    link Sign Up Now button to Modal;add css styling to site description

--- src/components/SiteDescription.jsx
+++ src/components/SiteDescription.jsx
@@ -1,7 +1,7 @@
 export default function SiteDescription() {
     return (
-        <div>
-            <h3>The World's Most Popular Sport</h3>
+        <div className="site-description">
+            <h3>Futbol! Soccer!</h3>
             <p>Introducing our futbol app -- your destination for all things futbol! Stay on top of the game with real-time access to comprehensive standings, up-to-date schedules, and exclusive insights. Whether you're a die-hard fan or a casual observer, our user-friendly interface ensures you never miss a goal, providing a seamless experience for tracking your favorite teams and staying in the loop with the latest futbol action.</p>
         </div>
     );
